e1a17c12dd82e13cd92bad50a07e9d9aaa694358
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-02-18 23:43:23 +0530

    working button

--- project/webapp/controller/App.controller.js
+++ project/webapp/controller/App.controller.js
@@ -1,15 +1,21 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller,
+	MessageToast) {
 		"use strict";
 
 		return Controller.extend("com.sap.demo.project.controller.App", {
 			onInit: function () {
 
+			},
+			onClick: function (oEvent) {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("buttonPage");
 			}
 		});
 	});
